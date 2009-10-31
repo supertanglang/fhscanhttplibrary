78e920930590de438e8b4cb5e866ac38629aad2c
atarasco@gmail.com <atarasco@gmail.com@b5171942-c3d4-11de-8b01-0f61d55110ad>
2009-10-31 17:50:48 +0000

    PREQUEST definition moved from HTTP.h to HTTPData.h

--- HTTPCore/trunk/HTTPCoreClass/HTTPData.h
+++ HTTPCore/trunk/HTTPCoreClass/HTTPData.h
@@ -3,6 +3,43 @@
 #include "FileMapping.h"
 #include "HTTPHANDLE.h"
 
+
+/*!\STRUCT PREQUEST
+  \brief This struct handles information related to and http response and includes information about client request, server response, url, server version .returned by an HTTP Server
+*/
+typedef struct  prequest {
+	HTTPCHAR hostname[256];
+   /*!< hostname of the server. This is related to the vhost parameter. If no vhost is specified, hostname contains the ip address. */
+   int ip;
+   /*!< remote HTTP ip address. */
+   int port;
+   /*!< remote HTTP port. This value is obtained from the InitHTTPConnectionHandle() */
+   int NeedSSL;
+   /*!< Boolean value. If this parameter is 1 then the connection is handled by openssl otherwise is just a tcp connection */
+   HTTPSTR url;
+   /*!< path to the file or directory requested */
+   HTTPSTR Parameters;
+   /*!< Request Parameters */
+   httpdata* request;
+   /*!< Information related to the HTTP Request. This struct contains both client headers and postdata */
+   httpdata* response;
+   /*!< Information related to the HTTP response. This struct contains both server headers and data */
+   HTTPSTR server;
+   /*!< pointer to a string that contains the server banner from the remote http server */
+   HTTPCHAR Method[20];
+   /*!< HTTP Verb used */
+   unsigned int status;
+   /*!< status code returned by the HTTP server. Example: "200", for an STATUS OK response. */
+   HTTPSTR ContentType;
+   /*!< Response Content-Type */
+public:
+   prequest();
+   ~prequest();
+   int IsValidHTTPResponse(void);
+   int HasResponseHeader(void);
+   int HasResponseData(void);
+} *PREQUEST;
+
 /*!\struct HTTP_DATA
   \brief An HTTP_DATA struct stores the information generated with an HTTP request or an HTTP response.\n
   If the data is related to an HTTP request, this struct will store the browser request headers and optional Post data.\n
